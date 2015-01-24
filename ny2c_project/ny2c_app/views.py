c74c06861224a78c0df89b85d2636e282b0be063
Ethan <ethan.long@vertical-knowledge.com>
2015-01-24 14:45:13 -0500

    first url route set up

--- ny2c_project/ny2c_app/views.py
+++ ny2c_project/ny2c_app/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response, RequestContext
+from ny2c_app.models import Pizza
 
+# Create your views here.
+def home(request):
+	pizzas = Pizza.objects.all()
+	context = {"pizzas":pizzas}
+	return render_to_response("home.html", locals(), context_instance=RequestContext(request))
 # Create your views here.
--- ny2c_project/ny2c_project/urls.py
+++ ny2c_project/ny2c_project/urls.py
@@ -3,8 +3,9 @@ from django.contrib import admin
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'ny2c_project.views.home', name='home'),
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', 'ny2c_app.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
 )
+
