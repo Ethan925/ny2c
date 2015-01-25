0a14115fa83f9cf06da2e3bee66c9a93d0e0f437
Ethan <ethan.long@vertical-knowledge.com>
2015-01-25 00:27:39 -0500

    creating order model

--- ny2c_project/ny2c_app/models.py
+++ ny2c_project/ny2c_app/models.py
@@ -1,6 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Order(models.Model):
+    table = models.CharField(max_length=50)
+    order_time = models.DateTimeField(auto_now_add=True)
+    def __unicode__(self):
+        return "%s." %(str(self.order_time))
+
 class Pizza(models.Model):
     NEW_YORK = 'New York'
     CHICAGO = 'CH'
@@ -16,14 +23,19 @@ class Pizza(models.Model):
     Pepperoni = 'Pepperoni'
     Sausage = 'Sausage'
     Onion = 'Onion'
+
     TOPPING_CHOICES = (
         (Pepperoni, 'Pepperoni'),
         (Sausage, 'Sausage'),
         (Onion, 'Onion'),
     )
+
     toppings = models.CharField(max_length=500,
                                       choices=TOPPING_CHOICES,
                                       default=Pepperoni)
     crust = models.CharField(max_length=8,
                                       choices=CRUST_CHOICES,
                                       default=NEW_YORK)
+
+    def __unicode__(self):
+        return "Make a %s with %s." %(self.crust, str(self.toppings))
