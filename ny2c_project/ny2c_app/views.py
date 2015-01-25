29327d87c0ed12d97fed83b955101802724add56
Ethan <ethan.long@vertical-knowledge.com>
2015-01-25 01:58:15 -0500

    classed base view

--- ny2c_project/ny2c_app/views.py
+++ ny2c_project/ny2c_app/views.py
@@ -1,10 +1,24 @@
 from django.shortcuts import render, render_to_response, RequestContext
 from ny2c_app.models import *
+from django.views.generic import View
 
-# Create your views here.
-def home(request):
-	pizzas = Pizza.objects.all()
-	orders = Order.objects.all()
-	context = {"pizzas":pizzas}
-	context = {"orders":orders}
-	return render_to_response("home.html", locals(), context_instance=RequestContext(request))
+# # Create your views here.
+# def home(request):
+# 	pizzas = Pizza.objects.all()
+# 	orders = Order.objects.all()
+# 	context = {"pizzas":pizzas}
+# 	context = {"orders":orders}
+# 	return render_to_response("home.html", locals(), context_instance=RequestContext(request))
+
+
+class HomeView(View):
+
+	template_name = "home.html"
+
+	def get(self, request, *args, **kwargs):
+		# form = NewPizzaForm()
+	 	pizzas = Pizza.objects.all()
+	 	orders = Order.objects.all()
+	 	context = {"pizzas":pizzas}
+	 	context = {"orders":orders}
+		return render_to_response(self.template_name, locals(), context_instance=RequestContext(request))
--- ny2c_project/ny2c_project/urls.py
+++ ny2c_project/ny2c_project/urls.py
@@ -1,11 +1,13 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from ny2c_app import views
 
 urlpatterns = patterns('',
     # Examples:
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', 'ny2c_app.views.home', name='home'),
+    # url(r'^$', 'ny2c_app.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
+    url(r'^$', views.HomeView.as_view(), name='home'),
 
 )
 
